6caad79c783e05d74288350431112948744af3f9
sarabelkacem <sarahbelkacem00000>
2024-09-24 17:18:04 +0200

    challenge

---
+++ Day01/Variables/Challenge1.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+int main() {
+    char first_name[20];
+    char last_name[20];
+    int age[2];
+    char sexe[10];
+    char email[20];
+    printf("enter your first name:");
+    scanf("%s",first_name);
+    printf("enter your last name:");
+    scanf("%s",last_name);
+    printf("how old are you?");
+    scanf("%d",age);
+    printf("enter your sexe:");
+    scanf("%s",sexe);
+    printf("enter your email:");
+    scanf("%s",email);
+    return 0;
+
+}
